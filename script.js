5b95da21502dee7e5585be38250c74aef0989a3d
WxcJJ26 <jjnkproxd@gmail.com>
2022-03-08 10:19:10 +0200

    pirmais

---
+++ script.js
@@ -0,0 +1,13 @@
+//var, let, const
+//console.log
+const vardi = ['Sandra Vera','Laura Petere','Anna Ločmele','Krišjānis Mednis','Rihards Reinfelds'];
+const balvas = ['dators','telefons','gludeklis','puķes','fotoaparāts'];
+const naudaskopa = 1000000;//sis pat netiks izmantots
+let uzvaretajuskaits = 5;
+/*ath.random=Math.random()*10
+rand= Math.floor()*/
+console.log(vardi[rand]);
+
+
+
+
