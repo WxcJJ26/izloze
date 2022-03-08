1ab49493539e14949ccbeaf0a200f6d17e8a9855
WxcJJ26 <jjnkproxd@gmail.com>
2022-03-08 11:14:50 +0200

    summa

--- script.js
+++ script.js
@@ -4,9 +4,26 @@ const vardi = ['Sandra Vera','Laura Petere','Anna Ločmele','Krišjānis Mednis'
 const balvas = ['dators','telefons','gludeklis','puķes','fotoaparāts'];
 const naudaskopa = 1000000;//sis pat netiks izmantots
 let uzvaretajuskaits = 5;
-/*ath.random=Math.random()*10
-rand= Math.floor()*/
-console.log(vardi[rand]);
+
+let rindas = document.querySelector('.rindas'); //pievieno mainīgo
+
+for (let i=0;i<uzvaretajuskaits;i++){//cikls, i++ paņem vienu uz preikšu un limits ir uzvaretaju skaits
+
+let rand = Math.random()*vardi.length; //vardu skaitu mainigais, kas tiks randomizets
+
+//Math.random=Math.random()*5
+//nejauši izvelas no 5
+rand= Math.floor(rand);//Math floor -  noapaļo uz leju
+
+let uzvaretajs = vardi[rand];
+//console.log(uzvaretajs);
+
+}
+rindas.innerHTML += ` 
+<tr>
+<td>${i+1}</td> //$ norada mainigo
+<td>${uzvaretajs}</td>
+</tr>`//izvada vairakas rindas
 
 
 
